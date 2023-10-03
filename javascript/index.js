92789c0245077d55d31010de7570309098e7fea3
Ayham Alahmad <86928915+Ayhamalahmad@users.noreply.github.com>
2023-10-03 20:07:58 +0300

    toggles themes and the "go-up" button

--- javascript/index.js
+++ javascript/index.js
@@ -1,20 +1,30 @@
-let dropdownMenuItems = document.querySelectorAll(".dropdown-menu li");
 let body = document.querySelector("body");
-
-dropdownMenuItems.forEach((item) => {
-  item.addEventListener("click", (e) => {
-    if (e.target.classList.contains("dark")) {
-      body.setAttribute('data-bs-theme','dark')
-    } else if (e.target.classList.contains("light")) {
-      body.setAttribute('data-bs-theme','light')
-    } 
-  });
+let themeTogler = document.querySelector(".theme-togler");
+let themeToglerIcon = document.querySelector(".theme-togler .icon");
+let themeToglerSpan = document.querySelector(".theme-togler span");
+// Adding a click event listener to the theme toggle button
+themeTogler.addEventListener("click", (e) => {
+      // If the moon icon is present, switch to dark theme
+  if (themeToglerIcon.classList.contains("fa-moon")) {
+    body.setAttribute("data-bs-theme", "dark");
+    themeToglerIcon.classList.remove("fa-moon");
+    themeToglerIcon.classList.add("fa-sun");
+    themeToglerSpan.textContent = "Light";
+  } else {
+      // If the sun icon is present, switch to light theme
+    body.setAttribute("data-bs-theme", "light");
+    themeToglerIcon.classList.remove("fa-sun");
+    themeToglerIcon.classList.add("fa-moon");
+    themeToglerSpan.textContent = "Dark";
+  }
 });
 
-  
-    // dropdownMenuItems.forEach((item) => {
-    //   item.addEventListener('click', () => {
-    //     const selectedTheme = item.classList.contains('dark') ? 'dark' : 'light';
-    //     document.documentElement.style.setProperty('--theme', selectedTheme);
-    //   });
-    // });
+let goUp= document.querySelector('.go-up');
+// Adding a scroll event listener to the window
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= 1000) {
+    goUp.classList.add("active");
+  } else {
+    goUp.classList.remove("active");
+  }
+});
